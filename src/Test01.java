b6ce2167c1ede05870f9728ca9340bfc2339d04d
lishuangbiao <1440770436@qq.com>
2020-12-08 21:45:23 +0800

    lambda

---
+++ src/Test01.java
@@ -0,0 +1,29 @@
+public class Test01 {
+    public static void main(String[] args) {
+        Test01 test01 = new Test01();
+        // 类型声明
+        MathOperation addition = (int a, int b) -> a + b;
+
+        // 不用类型声明
+        MathOperation subtraction = (a, b) -> a - b;
+
+        // 大括号中的返回语句
+        MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        // 没有大括号及返回语句
+        MathOperation division = (int a, int b) -> a / b;
+    }
+
+
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+    private int operate(int a, int b, MathOperation mathOperation){
+        return mathOperation.operation(a, b);
+    }
+}
